a3147023244a237186d724c0c13576142efadbde
minemine02 <minedemail@gmail.com>
2025-10-17 13:17:33 +0900

    バグ修正

--- src/main/java/com/minede/minememo/minememo/config/WebConfig.java
+++ src/main/java/com/minede/minememo/minememo/config/WebConfig.java
@@ -3,19 +3,18 @@ package com.minede.minememo.minememo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.lang.NonNull;
 
 @Configuration
 public class WebConfig {
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") //フロントのURLに応じて変更
+                        .allowedOrigins("http://localhost:5173") //フロントのURLに応じて変更
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
